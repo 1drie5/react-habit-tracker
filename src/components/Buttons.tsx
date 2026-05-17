0e3258e9db797b4a078e6ee5a6d784b2c1a6620b
Idries Ahamed K A <idriesahamedka@gmail.com>
2026-05-17 23:59:08 +0530

    add initial weekly habit item layout and button variants

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,8 +1,11 @@
-import type { ReactNode } from "react"
+import type { ComponentProps } from "react"
 
 type ButtonProps = {
-    children: ReactNode
-}
-export function Button({children}: ButtonProps) {
-    return <button className="bg-violet-600 hover:bg-violet-500 transition-colors rounded px-2 py-1 disabled:opacity-30 disabled:cursor-not-allowed">{children}</button>
+    variant: "primary" | "secondary" | "ghost-destructive"
+} & ComponentProps<"button">
+
+export function Button({ variant = "primary",...props }: ButtonProps) {
+    return <button 
+    {...props} 
+    className="bg-violet-600 hover:bg-violet-500 transition-colors rounded px-2 py-1 disabled:opacity-30 disabled:cursor-not-allowed"/>
 }
